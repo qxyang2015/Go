290a34891c957eb726fded9e7fd129933ade0659
qxyang <yangqingxiang@yxqiche.com>
2020-05-04 19:28:05 +0800

    更新demo

--- main.go
+++ main.go
@@ -2,20 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/qxyang2015/Go/struct_demo"
+	"github.com/qxyang2015/Go/gjson"
 )
 
-/*
-结构体函数不能实际改变结构体成员变量的值
-结构体指针函数可以改变成员变量的值
-*/
 func main() {
-	//调用结构体函数设置Name
-	sd := &struct_demo.StructDemo{"初始化Name"}
-	sd.SetName()
-	fmt.Println("name = ", sd.Name)
-	//调用结构体指针函数设置Name
-	sd1 := &struct_demo.StructDemo{"初始化Name"}
-	sd1.PointSetName()
-	fmt.Println("name = ", sd1.Name)
+	fmt.Println("start")
+	gjson.GojsonDemo()
+	fmt.Println("done!")
 }
