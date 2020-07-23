7e78898a076ca47342a19a78f5ecc74dbc8a22ef
qxyang <Yang>
2020-07-23 22:33:44 +0800

    启用go mod

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import gojsonq "github.com/thedevsaddam/gojsonq"
 
+//
 func main() {
 	const json = `{"name":{"first":"Tom","last":"Hanks"},"age":61}`
 	name := gojsonq.New().FromString(json).Find("name.first")
