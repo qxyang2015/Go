7d2ea1b18b66e163cc6f89311f7cee31b72aa25d
qxyang <yangqingxiang@yxqiche.com>
2020-08-18 14:33:18 +0800

    demo

--- main.go
+++ main.go
@@ -2,11 +2,34 @@ package main
 
 import (
 	"fmt"
-	"github.com/qxyang2015/Go/interface_demo"
+	"os"
+	"runtime/trace"
+	"sync"
 )
 
+func mockSendToServer(url string) {
+	fmt.Printf("server url: %s\n", url)
+}
+
 func main() {
-	fmt.Println("start")
-	interface_demo.Demo2()
-	fmt.Println("done!")
+	f, err := os.Create("trace.out")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	err = trace.Start(f)
+	if err != nil {
+		panic(err)
+	}
+	defer trace.Stop()
+	urls := []string{"0.0.0.0:5000", "0.0.0.0:6000", "0.0.0.0:7000"}
+	wg := sync.WaitGroup{}
+	for _, url := range urls {
+		wg.Add(1)
+		go func(url string) {
+			defer wg.Done()
+			mockSendToServer(url)
+		}(url)
+	}
+	wg.Wait()
 }
