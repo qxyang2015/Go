06bb6dd9f67b29c054bd3b411eeb5f2bf1ef3eab
qxyang <Yang>
2020-09-14 22:20:41 +0800

    调整项目目录

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
-import gojsonq "github.com/thedevsaddam/gojsonq"
+import (
+	"fmt"
+	"strconv"
+)
 
-//
+func FillMap(m map[string]string) {
+	for i := 1; i < 5; i++ {
+		s := strconv.Itoa(i)
+		m[s] = s
+	}
+}
+
 func main() {
-	const json = `{"name":{"first":"Tom","last":"Hanks"},"age":61}`
-	name := gojsonq.New().FromString(json).Find("name.first")
-	println(name.(string)) // Tom
+	m := make(map[string]string)
+	m["0"] = "a"
+	fmt.Println(m)
+	FillMap(m)
+	fmt.Println(m)
 }
