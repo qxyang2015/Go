c078d1c397cb0daf4b7080067783c1545942d614
qxyang <Yang>
2020-09-15 22:09:43 +0800

    开始练习题

--- utils/camel.go
+++ utils/camel.go
@@ -7,19 +7,11 @@ import (
 
 /*将字符转换为camel风格*/
 func Camel(strRaw string) string {
-
 	strList := strings.Split(strRaw, "_")
-
 	var charBuffer bytes.Buffer
-
 	for _, str := range strList {
-
 		str = strings.Title(str)
-
 		charBuffer.WriteString(str)
-
 	}
-
 	return charBuffer.String()
-
 }
