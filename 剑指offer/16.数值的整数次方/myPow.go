1c5d5b7a696c926c419bbafe69a8dc114e39a5b0
qxyang <Yang>
2020-09-23 22:37:58 +0800

    16更新

--- 剑指offer/16.数值的整数次方/myPow.go
+++ 剑指offer/16.数值的整数次方/myPow.go
@@ -7,7 +7,7 @@ func myPow(x float64, n int) float64 {
 	if n > 0 {
 		return pow(x, n)
 	} else {
-		return 1 / pow(x, -n)
+		return pow(1/x, -n)
 	}
 }
 
@@ -15,10 +15,9 @@ func pow(x float64, n int) float64 {
 	if n == 1 {
 		return x
 	}
-	v := myPow(x, n/2)
-	res := v * v
-	if n%2 == 1 {
-		res *= x
+	if n&1 == 1 {
+		return pow(x, n-1) * x
+	} else {
+		return pow(x*x, n>>1)
 	}
-	return res
 }
