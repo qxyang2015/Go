2052f6cd47cf8436508567490be17c6f9f2c300a
qxyang <Yang>
2020-10-08 22:29:01 +0800

    24更新

--- main.go
+++ main.go
@@ -2,9 +2,40 @@ package main
 
 import (
 	"fmt"
-	fib "github.com/qxyang2015/Go/剑指offer/10.斐波那契数列"
+	ReverseList "github.com/qxyang2015/Go/剑指offer/24.反转链表"
 )
 
 func main() {
-	fmt.Println(fib.Fib(10))
+	l := &ReverseList.ListNode{
+		Val: 1,
+		Next: &ReverseList.ListNode{
+			Val: 2,
+			Next: &ReverseList.ListNode{
+				Val: 3,
+				Next: &ReverseList.ListNode{
+					Val: 4,
+					Next: &ReverseList.ListNode{
+						Val:  5,
+						Next: nil,
+					},
+				},
+			},
+		},
+	}
+	ShowList(l)
+	resL := ReverseList.ReverseList(l)
+	ShowList(resL)
+	fmt.Println("Done!")
+}
+
+func ShowList(l *ReverseList.ListNode) {
+	node := l
+	for {
+		if node == nil {
+			break
+		}
+		fmt.Print(node.Val, " ")
+		node = node.Next
+	}
+	fmt.Println()
 }
