fa211b58f393d516bb4444d686012a9f773c95ca
qxyang <819705349@qq.com>
2020-12-02 23:07:16 +0800

    调整项目

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Done!")
+	const N = 1024
+	var a [N]int
+	x := append(a[:N-1:N], 0, 9)
+	fmt.Println(cap(a[:N-1:N]), len(a[:N-1:N]))
+	y := append(a[:N:N], 9)
+	fmt.Println(cap(a[:N:N]), len(a[:N:N]))
+	println(cap(x), cap(y))
 }
