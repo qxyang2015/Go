287d7bef02e00c3e8f9438cfcefc1712c118db54
qxyang <819705349@qq.com>
2020-12-14 22:33:04 +0800

    unsafe

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"net/http"
+	"path/filepath"
+)
 
 func main() {
-	const N = 1024
-	var a [N]int
-	x := append(a[:N-1:N], 0, 9)
-	fmt.Println(cap(a[:N-1:N]), len(a[:N-1:N]))
-	y := append(a[:N:N], 9)
-	fmt.Println(cap(a[:N:N]), len(a[:N:N]))
-	println(cap(x), cap(y))
+	context.Background()
+	http.HandleFunc()
+	http.NewServeMux()
+	http.ListenAndServe()
+	filepath.Walk()
 }
