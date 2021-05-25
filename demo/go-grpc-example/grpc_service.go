835977951d6a9556da40688fcd9fae634c5b5ce0
qxyang <819705349@qq.com>
2021-05-25 21:48:20 +0800

    调整代码

--- demo/go-grpc-example/grpc_service.go
+++ demo/go-grpc-example/grpc_service.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"demo/go-grpc-example/service"
+	"fmt"
 	"google.golang.org/grpc"
 	"log"
 	"net"
-	"service"
 )
 
 func main() {
@@ -15,11 +16,11 @@ func main() {
 	service.RegisterProdServiceServer(rpcServer, new(service.ProdService))
 
 	// 3. 新建一个listener，以tcp方式监听8082端口
-	listener, err := net.Listen("tcp", ":8082")
+	listener, err := net.Listen("tcp", ":8081")
 	if err != nil {
 		log.Fatal("服务监听端口失败", err)
 	}
-
+	fmt.Println("创建一个grpc服务")
 	// 4. 运行rpcServer，传入listener
 	_ = rpcServer.Serve(listener)
 }
